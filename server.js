13d2fd5c0d212c8a61800f89438740b4f6a47dd7
DylanC <88355961+comeaudc@users.noreply.github.com>
2024-03-22 15:54:28 -0400

    Initial Commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 require('dotenv').config();
 
 //Initialize our app variable with Express
@@ -10,6 +11,7 @@ connectDB();
 
 // Initialize middleware
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 //Single endpoint just to test API. Send data to browser
 // app.get('/', (req, res) => res.send('API Running'))
@@ -19,6 +21,6 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 // Enviromental Variables
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
